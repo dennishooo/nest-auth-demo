9a50aeb34867f9cac79cf695ad9a680a35b367b5
dennishooo <hkhdennis@gmail.com>
2023-07-07 15:11:06 +0800

    add cache manager

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,19 +1,28 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { jwtConstants } from './constants';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
+      ignoreExpiration: true,
       secretOrKey: jwtConstants.secret,
     });
   }
 
   async validate(payload: any) {
+    console.log({ payload });
+    const value = await this.cacheManager.get('key');
+    console.log({ value });
+    if (!value) {
+      await this.cacheManager.set('key', 'value');
+    }
+
     return { userId: payload.sub, username: payload.username };
   }
 }
